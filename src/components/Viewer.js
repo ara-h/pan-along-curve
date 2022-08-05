5b33f1b2376c1230e7b779d7858ffa6a7adbf145
Ara James Hanissian, Jr <68933196+ara-h@users.noreply.github.com>
2022-08-04 21:03:29 -0500

    Render animation and audio in one function

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,58 +1,99 @@
-import React from "react";
+import * as THREE from "three";
+import { Component, useEffect, useRef, useState } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
+// import { Text } from "@react-three/drei";
 import { curves } from "./Curves";
 
 import "./Viewer.css";
 
 
-function PointCurveAnimation(props) {
-  const myMesh = React.useRef();
+function AudioAndAnimation(props) {
+  const sound = useRef();
+  const emitterMesh = useRef();
+  const receiverMesh = useRef();
+
+  const [listener] = useState(() => new THREE.AudioListener());
+
+  const audioCtx = listener.context;
+  const oscillator = audioCtx.createOscillator();
+  oscillator.type = 'sine';
+  oscillator.frequency.setValueAtTime(220, audioCtx.currentTime);
+
+
+  useEffect(() => {
+    const receiver = receiverMesh.current;
+    receiver.add(listener);
+
+    sound.current.setNodeSource(oscillator);
+    oscillator.start();
+    console.log("oscillator started");
+
+    return () => {
+      receiver.remove(listener);
+      oscillator.stop();
+     }
+  }, [listener,oscillator])
 
   let p;
   let a = 0.0;
 
   useFrame((state, delta, xrFrame) => {
-    if(props.animate.current) {
+    if (props.animate.current) {
       a += delta;
 
-      p = props.f(a)
-      myMesh.current.position.x = p[0];
-      myMesh.current.position.y = p[1];
-      myMesh.current.position.z = p[2];
+      p = props.f(a);
+      emitterMesh.current.position.x = p[0];
+      emitterMesh.current.position.y = p[1];
+      emitterMesh.current.position.z = p[2];
     }
   })
 
-  return (
-    <mesh ref={myMesh}>
+  return(
+    <>
+    <mesh ref={emitterMesh}>
       <sphereBufferGeometry args={[0.1,16,16]} />
-      <meshStandardMaterial color="blue" />
+      <meshStandardMaterial color="blue"  />
+      <positionalAudio ref={sound} args={[listener]} />
     </mesh>
+    <mesh
+      ref={receiverMesh}
+      position={[0, 0, 0]}
+    />
+    <mesh>
+      <sphereBufferGeometry args={[0.995,16,16]} />
+      <meshStandardMaterial transparent opacity={0.4} color="red" />
+    </mesh>
+    </>
   )
 }
 
-class Viewer extends React.Component {
+
+
+class Viewer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animate: props.animate
+      animate: props.animate,
+      whichCurve: props.whichCurve,
+      curveFun: (curves.get(this.props.whichCurve)).fun
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.props.whichCurve !== prevState.whichCurve) {
+      this.setState({ whichCurve : (curves.get(this.props.whichCurve)).fun });
     }
   }
 
 
   render() {
-    const curve = (curves.get(this.props.whichCurve)).fun;
     return (
       <div className="Viewer">
         <Canvas>
-          <ambientLight args={[0xff0000]} intensity={0.1} />
-          <directionalLight position={[0, 0, 5]} intensity={0.5} />
-          <mesh>
-            <sphereBufferGeometry args={[0.995,16,16]} />
-            <meshStandardMaterial transparent opacity={0.4} color="red" />
-          </mesh>
-          <PointCurveAnimation
-            f={curve}
-            animate={this.state.animate} />
+          <AudioAndAnimation
+            f={this.state.curveFun}
+            animate={this.state.animate}
+          />
         </Canvas>
       </div>
     );
